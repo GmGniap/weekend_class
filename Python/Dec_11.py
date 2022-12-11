35670d957d610a24f36fbfbd124e0d8d96058917
WaiWlyan <hlyanw@yahoo.com>
2022-12-11 20:15:10 +0630

    Python_if

---
+++ Python/Dec_11.py
@@ -0,0 +1,10 @@
+icream = 800
+my_money = 600
+my_friend_money = 400
+
+if icream > my_money:
+    print("Expensive!")
+elif icream == my_friend_money == my_money:
+    print("Yahoooo!")
+else:
+    print("Let go home")     
